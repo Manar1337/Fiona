d6545de0dc61935ed4211db6805fdbeb3b740559
Björn Eriksson <bjoer348@gmail.com>
2024-05-03 07:00:54 +0200

    Fixed player moving slow

--- components/move_components/move_controlled_component.gd
+++ components/move_components/move_controlled_component.gd
@@ -2,17 +2,20 @@ class_name MoveControlledComponent
 extends Node
 
 @export var speed: float = 50.0
-@export var direction: Vector2 = Vector2.LEFT
 
+var direction: Vector2 = Vector2.ZERO
 var mode = "controlled"
 
 func calculate_movement(delta):
+	set_direction(Vector2.ZERO)
 	var input_axisX = Input.get_axis("ui_left","ui_right")
 	var input_axisY = Input.get_axis("ui_up","ui_down")
+
 	if input_axisX < 0 : set_direction(Vector2.LEFT)
 	if input_axisX > 0 : set_direction(Vector2.RIGHT)
 	if input_axisY < 0 : set_direction(Vector2.UP)
 	if input_axisY > 0 : set_direction(Vector2.DOWN)
+	print(direction)
 	return (direction * delta).normalized() * speed
 
 func set_data(data: Array):
